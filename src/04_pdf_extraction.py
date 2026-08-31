02a96bc92c1e770ae0d1f512655ebb6957df4ace
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-08-31 12:32:51 +0530

    Remove numbered Python file

--- src/04_pdf_extraction.py
+++
@@ -1,7 +0,0 @@
-from pypdf import PdfReader
-
-
-def extract_pdf_text(pdf_path: str, first_page_only: bool = True) -> str:
-    reader = PdfReader(pdf_path)
-    pages = reader.pages[:1] if first_page_only else reader.pages
-    return "".join(page.extract_text() or "" for page in pages)
