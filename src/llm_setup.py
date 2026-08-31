03beef66d3366bce09b37232ee4101d232921d51
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-08-31 13:11:00 +0530

    Delete src/llm_setup.py

--- src/llm_setup.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-
-def load_chatbot():
-    chatbot = pipeline(
-        "text-generation",
-        model="microsoft/Phi-3-mini-4k-instruct"
-    )
-    return chatbot
